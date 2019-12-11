98f959bae4500974696c0b788b1ee7abd06ff064
wangjian <245186716@qq.com>
2019-12-11 10:46:01 +0800

    create rabbitMQ pom

---
+++ ecs-rabbitmq/src/main/java/com/finstone/rabbitmq/EcsRabbitmqApplication.java
@@ -0,0 +1,13 @@
+package com.finstone.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcsRabbitmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcsRabbitmqApplication.class, args);
+	}
+
+}
---
+++ ecs-rabbitmq/src/test/java/com/finstone/rabbitmq/EcsRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finstone.rabbitmq;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcsRabbitmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
