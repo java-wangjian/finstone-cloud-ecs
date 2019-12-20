42725951ade355abafd5d01afcfaa16fd08fa800
wangjian <245186716@qq.com>
2019-12-20 13:30:23 +0800

    update

---
+++ ecs-common/src/main/java/com/finstone/common/StringTest.java
@@ -0,0 +1,15 @@
+package com.finstone.common;
+
+/**
+ * Author: wangjian
+ * Date:   19-12-20
+ * Time:   下午1:29
+ * Description:
+ */
+public class StringTest {
+    static String s = "2222";
+
+    public static void main(String[] args) {
+        System.out.println(s);
+    }
+}
